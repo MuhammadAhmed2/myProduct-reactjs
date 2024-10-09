8e16076766f8061d0a0d44cb68ee63aca4aaabc3
MuhammadAhmed2 <exact.ahmed@gmail.com>
2024-10-09 16:55:15 +0500

    100%

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import './App.css'; 
 
 const arr = [
@@ -21,22 +22,42 @@ const arr = [
 
 
 
+
+
+
+
 const App = () => {
+  
+  const [Name, setName] = useState('mango')
+  const [Price, setPrice] = useState('mango')
+  const [Img, setImg] = useState('mango')
+
+  function update(index) {
+    console.log(index);
+
+  setName(arr[index].productName)
+  setPrice(arr[index].price)
+  setImg(arr[index].imgUrl)
+  }
+
+  
   return (
     <>
-      <img className='Main-product-image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEtYjB0kutsOvPYCvAV1_rwSZT5Sw-rNfAcg&s" />
+      <img className='Main-product-image' src={Img}/>
       <div>
-        <h1 className='product-title'>productName</h1>
-        <p className='price'>price</p>
-        <div className='crousel-selection'>
-          <button><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEtYjB0kutsOvPYCvAV1_rwSZT5Sw-rNfAcg&s" alt="" /></button>
-          <button><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQcRfIW5tggAic_wBYqqZstYQzyvi407PIDA&s" alt="" /></button>
-          <button><img src="https://www.fourwindsgrowers.com/cdn/shop/files/Pomona-Sweet-Semi-Dwarf-Lemon-Tree_1024x1024.jpg?v=1696267527" alt="" /></button>
+        <h1 className='product-title'>{Name}</h1>
+        <p className='price'>{Price}</p>
+        <div  className='crousel-selection' >
+        {arr.map((items, index)=>{
+          return <div key={index}>
+                  <button><img onClick={() => update(index)} src={items.imgUrl}/></button>
+                 </div>
+        })}
         </div>
         <button>Buy Now</button>
-      </div>
-    </>
+      </div>  
+    </>    
   )
-}
+}  
 
 export default App
